6d3975d29975ff574a6c9c5f9ebc597f3fd84095
Harshit Vijay <harshitvijay007@gmail.com>
2021-09-23 09:31:55 +0530

    Updated PastReminderList Component

--- src/Components/PastReminderList/index.js
+++ src/Components/PastReminderList/index.js
@@ -1,32 +1,8 @@
 import React from "react";
-import { deleteReminder } from "../../Redux/Actions";
-import { connect } from "react-redux";
+import ReminderList from "../ReminderList";
 
 const PastReminderList = ({ reminder, index }) => {
-  const deleteReminderRow = (index) => {
-    deleteReminder(index);
-  };
-  return (
-    <div className="bg-secondary bg-gradient text-light rounded p-1 m-1 position-relative">
-      <h3 className="text-center">{reminder.task}</h3>
-      <div className="d-flex justify-content-evenly text-center">
-        <p>{reminder.date}</p>
-        <p>{reminder.time}</p>
-      </div>
-      <button
-        type="button"
-        className="btn-close position-absolute top-0 right-0 btn-danger"
-        aria-label="Close"
-        onClick={() => {
-          deleteReminderRow(index);
-        }}
-      ></button>
-    </div>
-  );
+  return <ReminderList reminder={reminder} index={index} />;
 };
 
-const mapStateToProps = (state) => ({
-  reminders: state,
-});
-
-export default connect(mapStateToProps, { deleteReminder })(PastReminderList);
+export default PastReminderList;
