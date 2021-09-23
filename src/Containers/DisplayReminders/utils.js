55a8f0023bc98bffb1a43ef65ce28f026f13a512
Harshit Vijay <harshitvijay007@gmail.com>
2021-09-23 09:28:50 +0530

    Fixes isPastTime Function

--- src/Containers/DisplayReminders/utils.js
+++ src/Containers/DisplayReminders/utils.js
@@ -2,12 +2,13 @@ export const isPastTime = (date, time) => {
   let isPast = false;
   let myDate = date;
   myDate = myDate.split("-");
-  const newDate = new Date(myDate[2], myDate[1] - 1, myDate[0]);
+  const newDate = new Date(myDate[0], myDate[1] - 1, myDate[2]);
+  myDate = newDate.getTime();
   let myTime = time;
   myTime = myTime.split(":");
-  const newTime =
-    (myTime[0] * 3600 + myTime[1] * 60) * 1000 + newDate.getTime();
-  if (Date.now() > newTime) {
+  const newTime = (myTime[0] * 3600 + myTime[1] * 60) * 1000;
+  const actualTime = myDate + newTime;
+  if (Date.now() > actualTime) {
     isPast = true;
   }
   return isPast;
