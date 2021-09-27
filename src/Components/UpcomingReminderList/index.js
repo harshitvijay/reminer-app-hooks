e2c6412a96b378cba5d6adce9612fa72f97d03de
Harshit Vijay <harshitvijay007@gmail.com>
2021-09-27 10:57:46 +0530

    Code Optimised

--- src/Components/UpcomingReminderList/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReminderList from "../ReminderList";
-
-const UpcomingReminderList = ({ reminder, index }) => {
-  return <ReminderList reminder={reminder} index={index} />;
-};
-
-export default UpcomingReminderList;
